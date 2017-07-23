93a45613b33840e7346bbb8aae2184406892c8c6
wangbiaozy <56565283@qq.com>
2017-07-24 00:54:30 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,21 @@
+(function(global){
+    function remChange(){
+        document.documentElement.style.fontSize=20*document.documentElement.clientWidth/750+'px';
+    }
+    remChange();
+    global.addEventListener('resize',remChange,false);
+})(window);
+
+$(function () {
+    
+    //显示遮罩层
+    function showMask(){
+        $("#mask").css("height",$(document).height());
+        $("#mask").css("width",$(document).width());
+        $("#mask").show();
+    }
+    //隐藏遮罩层
+    function hideMask(){
+        $("#mask").hide();
+    }
+});
